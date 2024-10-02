200dabb310e0887a94b06e33492b3d568a65b853
mnassrib <baligh.mnassri@gmail.com>
2024-10-02 11:45:56 +0200

    test2 CI/CD workflow

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,20 +8,15 @@ def client():
     with app.test_client() as client:
         yield client
 
-# def test_predict(client):
-#     # Simule une requête POST avec des données valides pour tester l'endpoint /predict
-#     response = client.post('/predict', json={
-#         'features': [5.1, 3.5, 1.4, 0.2]
-#     })
-    
-#     # Vérifie que le statut de la réponse est 200 (succès)
-#     assert response.status_code == 200
-    
-#     # Vérifie que la réponse contient bien une prédiction
-#     data = json.loads(response.data)
-#     assert 'prediction' in data
-
-
 def test_predict(client):
-    # Erreur intentionnelle dans le test pour faire échouer le pipeline
-    assert 1 == 2
+    # Simule une requête POST avec des données valides pour tester l'endpoint /predict
+    response = client.post('/predict', json={
+        'features': [5.1, 3.5, 1.4, 0.2]
+    })
+    
+    # Vérifie que le statut de la réponse est 200 (succès)
+    assert response.status_code == 200
+    
+    # Vérifie que la réponse contient bien une prédiction
+    data = json.loads(response.data)
+    assert 'prediction' in data
