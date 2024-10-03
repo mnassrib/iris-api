4ec967e7d3bf8a71025d375e83015c38047034ed
mnassrib <baligh.mnassri@gmail.com>
2024-10-03 17:29:38 +0200

    fix dynamic configuration

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ def index():
 
 if __name__ == '__main__':
     # Configurations pour l'exécution de l'application
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
